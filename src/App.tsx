c8e7cc87ad262966d450c769eb3117a8e2d2e298
Kevin Taber <taber64@gmail.com>
2023-11-10 23:10:43 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SampleTable from './Table/SampleTable'
+
+function App() {
+
+  return (
+    <>
+      <SampleTable />
+    </>
+  )
+}
+
+export default App
---
+++ src/Table/SampleTable.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+
+const columns = [
+  { field: 'id', headerName: 'ID' },
+  { field: 'title', headerName: 'Title', width: 300 },
+  { field: 'body', headerName: 'Body', width: 600 }
+]
+
+const SampleTable = () => {
+
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => data.json())
+      .then((data) => setTableData(data))
+
+  }, [])
+
+  console.log(tableData)
+
+  return (
+    <div>
+      <DataGrid
+        rows={tableData}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10, 25, 50, 100]}
+      />
+    </div>
+  )
+}
+
+export default SampleTable
